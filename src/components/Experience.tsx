01a0bdf5ff7f08ea478d0adcf7c25eb816bbf907
Florencia Canete <flormalenca0103@gmail.com>
2026-01-06 17:00:08 -0300

    Se agrego id

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,7 +2,7 @@ import TimeLine from './TimeLine';
 
 export default function Experiencie() {
   return (
-    <section className='flex items-center flex-col gap-20 bg-[#FFF7F5] py-20 md:py-28'>
+    <section id="experience" className='flex items-center flex-col gap-20 bg-[#fffff] py-10'>
         <div className="max-w-4xl mx-auto px-4">
             <p className="text-[#181311] text-4xl font-black leading-tight tracking-[-0.015em] pb-12 text-center">Experiencia</p>
 
