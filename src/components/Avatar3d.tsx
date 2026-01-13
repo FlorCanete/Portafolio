969756ec90c755e384fdb2882d25713b85d1bf39
Florencia Canete <flormalenca0103@gmail.com>
2026-01-13 00:31:48 -0300

    Modificacion de avatar 3d en el hero mobile

--- src/components/Avatar3d.tsx
+++ src/components/Avatar3d.tsx
@@ -1,18 +1,20 @@
 export default function Avatar3D() {
+  const isMobile = window.innerWidth < 768;
   return (
     <div
   id="container"
   style={{
+    height: "100%",
     width: "90%",
-    maxWidth: "500px",
+    maxWidth: "1600px",
     aspectRatio: "1 / 1",
     margin: "0 auto",
-    minHeight: "300px"  // asegura que haya altura mínima
   }}
 >
   <spline-viewer
     url={`${import.meta.env.BASE_URL}spline/scene.splinecode`}
     class="Spline-Avatar"
+    style={{ transform: isMobile ? 'scale(0.9)' : 'scale(0.8)' }}
   />
 </div>
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 
 export default function Hero() {
   return (
-    <section className='flex flex-col lg:flex-row items-center justify-center h-screen bg-gradient-to-b from-[#FFD6CC] via-[#FFEDEA] to-[#FFFFFF] pb-10' id='Home'>
+    <section className='flex flex-col lg:flex-row items-center justify-center min-h-screen bg-gradient-to-b from-[#FFD6CC] via-[#FFEDEA] to-[#FFFFFF] pb-10' id='Home'>
         <div className='flex flex-col items-center justify-center text-center lg:text-left gap-4 lg:gap-5 p-4 order-2 lg:order-1 '>
           <div className="inline-block px-4 py-1 rounded-full bg-white/50 border border-gray-200 backdrop-blur-sm mb-2">
             <span className="text-sm font-semibold text-center  tracking-wider uppercase text-[#FF7A7A]">👋 Bienvenido a mi portafolio</span>
@@ -16,14 +16,13 @@ export default function Hero() {
           </p>
 
           <motion.p
-          animate={{ scale: 2 }}
-            className="font-semibold text-2xl text-gray-400 text-shadow-md text-shadow-gray-300">
+            animate={{ scale: 2 }}
+            className="font-semibold text-xl md:text-2xl text-gray-400 text-shadow-md text-shadow-gray-300">
             Frontend Developer
           </motion.p>
 
           <div className='flex gap-3'>
             <Button name="Proyecto" href="#Projects" bg-color="bg-[#FF715B]"/>
-            <Button name="Contacto" href="#Contact" bg-color="bg-[#FF715B]"/>
           </div>
         </div>
 
